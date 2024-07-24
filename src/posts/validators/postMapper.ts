bd32d2bf7ac7ccfc5f86e91e2fd0ce88919cab48
AGUACATE-ONLINE <alan.castillo.e@gmail.com>
2024-07-24 11:51:27 +0200

    change validation naming

--- src/posts/validators/postMapper.ts
+++ src/posts/validators/postMapper.ts
@@ -1,7 +1,7 @@
 export function validatePost(post) {
   if (typeof post !== "object" || post === null) return false;
 
-  const { title, description } = post;
+  const { title, problem_description, solution_description } = post;
 
-  return typeof title === "string" && typeof description === "string";
+  return typeof title === "string" && typeof problem_description === "string" && typeof solution_description === "string";
 }
